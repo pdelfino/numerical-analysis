f34dc599b586a1e30fd7d367c61fa960f0b4414f
Pedro Delfino <p.delfino01@gmail.com>
2018-09-11 14:41:33 -0300

    still trying to build hm3

--- lista-3/2-questao.py
+++ lista-3/2-questao.py
@@ -1,12 +1,13 @@
-# implementar divided diferences
 from sympy import poly as p
 from sympy.abc import x
+import numpy as np
+import matplotlib.pylab as plot
 
-lista_exemplo = [ [0,0], [1,1], [2,2], [3,3]]
-
+lista_exemplo = [[0,0],[1,1], [2,2], [3,3]]
 exemplo1 = [[0,-1]]
 exemplo2 = [[0,-1],[1,1]]
 exemplo3 = [[0,-1],[1,1],[2,5]]
+exemplo_livro = [[1,3],[3/2,13/4],[0,3],[2,5/3]]
 
 def coefficient(lista):
 
@@ -20,42 +21,92 @@ def coefficient(lista):
 
         return (coefficient(lista[1:]) - coefficient(lista[:len_sub_1]))/(lista[len_sub_1][0]-lista[0][0])
 
-#print (coefficient(lista_exemplo))
-#print (coefficient([[0,1]]))
+def lista_coeficientes(l_p):
+   
+    lista_coef = []
+    
+    for i in range(1,len(l_p)+1):
+        
+        elemento = (coefficient(l_p[:i]))
+        lista_coef.append(elemento)
 
-print (coefficient(lista_exemplo))
-print (coefficient(exemplo2))
+    return lista_coef
 
-a_0 = coefficient(exemplo1)
-a_1 = coefficient(exemplo2)
-a_2 = coefficient(exemplo3)
+#print (lista_coeficientes(exemplo_livro))
 
-print (a_0,a_1,a_2)
+def caso_dois(x, l_p):
+    
+    c_f = lista_coeficientes(l_p)
+    
+    total=  (c_f[0]
+            + c_f[1]*(x-l_p[0][0])
+            + c_f[2]*(x-l_p[0][0])*(x-l_p[1][0]))
 
-print (p(x*(x - 1)**2))
+    return total
 
-def divided_diferences(lista_10):
 
-    a_0 = coefficient(lista_10[:1])
-    print(lista_10[:1])
-    a_1 = coefficient(lista_10[:2])
-    a_2 = coefficient(lista_10[:3])
-    print (lista_10[:3])
-    a_3 = coefficient(lista_10[:4])
-    a_4 = coefficient(lista_10[:5])
-    a_5 = coefficient(lista_10[:6])
-    a_6 = coefficient(lista_10[:7])
-    a_7 = coefficient(lista_10[:8])
-    a_8 = coefficient(lista_10[:9])
-    a_9 = coefficient(lista_10[:10])
+def caso_tres(x, l_p):
+    
+    c_f = lista_coeficientes(l_p)
+    
+    total=  (c_f[0]
+            + c_f[1]*(x-l_p[0][0])
+            + c_f[2]*(x-l_p[0][0])*(x-l_p[1][0])
+            + c_f[3]*(x-l_p[0][0])*(x-l_p[1][0])*(x-l_p[2][0]))
 
-    print (a_9,a_8,a_7,a_6,a_5,a_4,a_1,a_2,a_3)
+    return total
 
-lista_fodase = []
+#print (caso_tres(2, exemplo_livro))
+print (caso_dois(0, exemplo3))
 
-for i in range(1,11):
-    lista_fodase.append([i+4,i+1])
+def sample_cosine():
+    
+    x_vals = np.linspace(-1,1,10)
 
-#print (lista_fodase)
+    lista = []
 
-print (divided_diferences(lista_fodase))
+    for val in x_vals:
+
+        lista.append([val,np.cos(np.pi*val)])
+
+    return lista
+
+#print (sample_cosine())
+pontos_cosseno = sample_cosine()
+#print (lista_coeficientes(pontos_cosseno))
+
+def interpolate_10(x, l_p):
+   
+    # c_f means coeficient list
+    c_f = lista_coeficientes(l_p)
+    
+    init = c_f[0]
+    one = c_f[1]*(x-l_p[0][0])
+    two = c_f[2]*(x-l_p[0][0])*(x-l_p[1][0])
+    third = c_f[3]*(x-l_p[0][0])*(x-l_p[1][0])*(x-l_p[2][0])
+    fourth = c_f[4]*(x-l_p[0][0])*(x-l_p[1][0])*(x-l_p[2][0])*(x-l_p[3][0])
+    fifth = c_f[5]*(x-l_p[0][0])*(x-l_p[1][0])*(x-l_p[2][0])*(x-l_p[3][0])*(x-l_p[4][0])
+    sixth = c_f[6]*(x-l_p[0][0])*(x-l_p[1][0])*(x-l_p[2][0])*(x-l_p[3][0])*(x-l_p[4][0])*(x-l_p[5][0])
+    sventh = (c_f[7]*(x-l_p[0][0])*(x-l_p[1][0])*(x-l_p[2][0])*(x-l_p[3][0])*(x-l_p[4][0])*(x-l_p[5][0])*(x-l_p[6][0]))
+    eigth = ((c_f[8]*(x-l_p[0][0])*(x-l_p[1][0])*(x-l_p[2][0])*(x-l_p[3][0])*(x-l_p[4][0])*(x-l_p[5][0])*(x-l_p[6][0]))*(x-l_p[7][0]))
+    nine = ((c_f[8]*(x-l_p[0][0])*(x-l_p[1][0])*(x-l_p[2][0])*(x-l_p[3][0])*(x-l_p[4][0])*(x-l_p[5][0])*(x-l_p[6][0]))*(x-l_p[7][0])**(x-l_p[8][0]))
+ 
+
+
+    total = init+one+two+third+fourth+fifth+sixth+sventh+eigth+nine
+
+    return total
+#gerar pontos da função cos pix
+x = np.linspace(-1,1,10)
+#print (x)
+
+#print (interpolate_10(1/2, pontos_cosseno))
+
+#plot.plot(x,np.cos(np.pi*x))
+
+#print (interpolate_10(0.5,pontos_cosseno))
+#plot.plot(x,interpolate_10(x,pontos_cosseno))
+
+#plot.show()
+
+

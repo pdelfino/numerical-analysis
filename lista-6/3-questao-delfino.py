b5c1ee8994d7d985dca87dacde5b2f054cd30ed7
Pedro Delfino <p.delfino01@gmail.com>
2018-10-01 20:19:31 -0300

    agora sim, implementação boa

--- lista-6/3-questao-delfino.py
+++ lista-6/3-questao-delfino.py
@@ -64,7 +64,7 @@ def heun(x):
 
 #print (heun(1.5),g(1.5))
 
-t = np.linspace(-1,3, 18)
+t = np.linspace(-1,3, 30)
 
 lista_outputs = []
 
@@ -72,8 +72,53 @@ for i in t:
     lista_outputs.append(heun(i))
     #print (i)
 
+def euler(x):
+    
+    y_init = 1
+    x_init = 0
+    
+    old_dy_dx = y_init
+
+    old_y = y_init 
+
+    new_y = None
+    
+    new_dy_dx = None
+
+    
+    limite = 0
+
+    while x>limite:
+
+        #for i in range(1,6):
+        
+        new_y = h*old_dy_dx + old_y
+        #print ("new_y", new_y)
+
+        new_dy_dx = new_y
+        #print ("new dy_dx", new_dy_dx)
+
+        old_y = new_y
+        #print ("old_y", old_y)
+
+        old_dy_dx = new_y
+        #print ("old delta y_delta x", old_dy_dx)
+        #print ("iterada",i)
+        
+        limite = limite +h
+
+    return new_y
+
+lista_outputs_eul = []
+
+for i in t:
+    lista_outputs_eul.append(euler(i))
+    #print (i)
+
 plt.plot(t, g(t), 'b-', label='Output resultado analítico')
-plt.plot(t , lista_outputs, 'ro', label="Output resultado numérico")
+plt.plot(t , lista_outputs, 'ro', label="Output resultado numérico com Heun")
+plt.plot(t , lista_outputs_eul, 'go', label="Output resultado numérico com Euler clássico")
 plt.title('Comparação Euler/Analítico - tolerância: ' + str(h))
 pylab.legend(loc='upper left')
 plt.show()
+

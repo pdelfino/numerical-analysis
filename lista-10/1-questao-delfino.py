a6fd4a191416a5956d9aef78a73656b0051677e0
Pedro Delfino <p.delfino01@gmail.com>
2018-10-28 14:41:43 -0300

    add lista 10

---
+++ lista-10/1-questao-delfino.py
@@ -0,0 +1,53 @@
+import math
+
+n = 10
+
+m = 20
+
+h = 0.1
+
+k = 0.005
+
+u_0 = 0
+
+v_0 = 0
+
+u_n = 0
+
+v_n = 0
+
+lista_u_i = []
+
+for i in range(0, n):
+
+    lista_u_i.append(math.sin(math.pi*i*h))
+
+#print (len(lista_u_i))
+
+lista_v_i = []
+
+nova_u_i = []
+
+for j in range(0, m):
+
+    for i in range(0,n-1):
+        
+        val = (lista_u_i[i]+lista_u_i[i+1])/2
+
+        lista_v_i.append(val)
+
+    t=j*k
+
+    for i_two in range(0,n-1):
+        
+        val = (math.e)**((-1)*((math.pi)**2)*t)
+        val = (val*math.sin(math.pi*i*h)) - (lista_v_i[i_two])
+        nova_u_i.append(val)
+
+    for i_three in range(0,n-1):
+
+        lista_u_i[i_three]=lista_v_i[i_three]
+
+#print (len(lista_v_i))
+#print (len(nova_u_i))
+print (len(lista_u_i),lista_u_i)
